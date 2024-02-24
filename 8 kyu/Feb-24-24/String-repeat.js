d16637790eddd4526487a53ff241e3392c3bc16c
FranLi7 <franciscolimon1200@gmail.com>
2024-02-24 13:35:39 -0800

    Refactor code for improved performance

---
+++ 8 kyu/Feb-24-24/String-repeat.js
@@ -0,0 +1,22 @@
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// 0. a function that returns a string repeated by the number param
+// 1. I need to use a for loop to solve this problem
+// 2. The loop will run 'n' times
+// 3. Concatenate the 's' to the empty result variable.
+
+function stringRepeat(n, s) {
+  result = '';
+  for (let i = 0; i < n; i++) {
+    result += s;
+  }
+  return result;
+}
+
+//sample tests
+console.log(stringRepeat(10, ' hello world'));
+
+// Other solutions
+function stringRepeat2(n, s) {
+  return s.repeat(n);
+}
