b4eddf82f92d04bccb29285b436359b3c2e24f42
FranLi7 <franciscolimon1200@gmail.com>
2024-02-27 15:26:16 -0800

    solve simple multiplication problem

---
+++ 8 kyu/feb-27-24/simple-multiplication.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(number) {
+  return number % 2 === 0 ? number * 8 : number * 9;
+}
+
+console.log(simpleMultiplication(2));
+console.log(simpleMultiplication(3));
+console.log(simpleMultiplication(4));
